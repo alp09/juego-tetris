4cc4a054592ba15de087cd040935f1dda8559761
alp09 <abellp09.3@gmail.com>
2021-05-13 13:45:32 +0200

    Se añadió opción para saltar la PantallaSplash.

--- core/src/main/java/proyectojuego/pantallas/PantallaSplash.java
+++ core/src/main/java/proyectojuego/pantallas/PantallaSplash.java
@@ -1,5 +1,7 @@
 package proyectojuego.pantallas;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -10,14 +12,14 @@ public class PantallaSplash extends Pantalla {
 		AL USAR EL DELTA, LA ANIMACION DURA 1s PUESTO QUE VA SUMANDO ms A progresoAnimacion HASTA LLEGAR AL TOPE (1).
 		SE PODRIA MODIFICAR MULTIPLICANDO progresoAnimacion, POR EJEMPLO, AL MULTIPLICARLO POR .5, TARDARÍA 2s EN LLEGAR AL TOPE DE 1.
 	 */
-	public static final int TIEMPO_MINIMO_ENTRE_ANIMACIONES		= 0;	// TIEMPO EN SEGUNDOS ENTRE LAS ANIMACIONES
-	public static final int MULTIPLICADOR_ANIMACION_FADE_IN		= 4;	// <1 --> ANIMACION MAS LENTA
-	public static final int MULTIPLICADOR_ANIMACION_FADE_OUT	= 4;	//	1 --> VELOCIDAD NORMAL
+	public static final int TIEMPO_MINIMO_ENTRE_ANIMACIONES		= 2;	// TIEMPO EN SEGUNDOS ENTRE LAS ANIMACIONES
+	public static final int MULTIPLICADOR_ANIMACION_FADE_IN		= 1;	// <1 --> ANIMACION MAS LENTA
+	public static final int MULTIPLICADOR_ANIMACION_FADE_OUT	= 1;	//	1 --> VELOCIDAD NORMAL
 																		// >1 --> ANIMACION MAS RAPIDA
 
-	private float	progresoAnimacionFadeIn 	= 0;					// LA ANIMACION COMIENZA CON EL VALOR ALPHA A 0.
-	private float	progresoAnimacionFadeOut 	= 1;					// LA ANIMACION COMIENZA CON EL VALOR ALPHA A 1.
-	private float	tiempoEntreAnimaciones		= 0;					// DEFINE EL TIEMPO QUE HA TRANSCURRIDO DESDE QUE LA ANIMACION FADE IN TERMINÓ.
+	private float progresoAnimacionFadeIn 	= 0;						// LA ANIMACION COMIENZA CON EL VALOR ALPHA A 0.
+	private float progresoAnimacionFadeOut 	= 1;						// LA ANIMACION COMIENZA CON EL VALOR ALPHA A 1.
+	private float tiempoEntreAnimaciones	= 0;						// DEFINE EL TIEMPO QUE HA TRANSCURRIDO DESDE QUE LA ANIMACION FADE IN TERMINÓ.
 
 	private final Sprite	spritePantallaSplash;
 
@@ -40,6 +42,10 @@ public class PantallaSplash extends Pantalla {
 	@Override
 	public void gestionarInput(float delta) {
 
+		if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY) && assetManager.update()) {
+			juego.cambiarPantalla(this, new PantallaMenu());
+		}
+
 	}
 
 	@Override
@@ -48,7 +54,7 @@ public class PantallaSplash extends Pantalla {
 		if (progresoAnimacionFadeIn < 1) {
 			progresoAnimacionFadeIn += delta * MULTIPLICADOR_ANIMACION_FADE_IN;
 			spritePantallaSplash.setAlpha(Math.min(1, progresoAnimacionFadeIn));
-		} else if (assetManager.update() && tiempoEntreAnimaciones > TIEMPO_MINIMO_ENTRE_ANIMACIONES) {
+		} else if (tiempoEntreAnimaciones > TIEMPO_MINIMO_ENTRE_ANIMACIONES && assetManager.update()) {
 			tiempoEntreAnimaciones += delta;
 			progresoAnimacionFadeOut -= delta * MULTIPLICADOR_ANIMACION_FADE_OUT;
 			spritePantallaSplash.setAlpha(Math.max(0, progresoAnimacionFadeOut));
